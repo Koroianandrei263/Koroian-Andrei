4345e6c0aec349efcb223865c2038701f6fac649
Koroianandrei263 <akoroian26@gmail.com>
2024-10-22 22:04:08 +0300

    Update tema2.py

--- tema2.py
+++ tema2.py
@@ -1,5 +1,4 @@
 sir_articol="""O șefă dintr-o mare companie de telefonie mobilă nu doar că a scotocit în istoricul convorbirilor telefonice purtate de o clientă, dar le-a și divulgat iubitului acesteia, care încerca să afle astfel dacă este înșelat.
-
 Ilegalitățile au fost descoperite întâmplător, într-un alt caz scandalos de corupție. Angajata companiei și-a recunoscut fapta pentru a scăpa de detenție."""
 jumatate = len(sir_articol) // 2
 prima_parte = sir_articol[:jumatate]
